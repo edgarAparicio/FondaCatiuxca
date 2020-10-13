de48b669debc45369d918cea2c81a5bde8af90c3
Edgar Guillermo Aparicio Haro <edgar_chato@live.com.mx>
2020-10-13 10:54:49 -0500

    Se agregan vistas y los metodos al Controlador del detalle y la edicion de los menus

--- FondaCatiuxcaMVC/Controllers/HomeController.cs
+++ FondaCatiuxcaMVC/Controllers/HomeController.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EdgarAparicio.FondaCatiuxca.Business.Entity;
+using EdgarAparicio.FondaCatiuxca.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +13,54 @@ namespace FondaCatiuxcaMVC.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IMenu menuRepositorio;
+        private readonly IHtmlHelper htmlHelper;
+
+        [BindProperty]
+        public Menu Menu { get; set; }
+        public IEnumerable<SelectListItem> TipoComidaSelectList { get; set; }
+
+        public HomeController(IMenu menu, IHtmlHelper htmlH)
+        {
+            menuRepositorio = menu;
+            htmlHelper = htmlH;
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            var listaMenus = menuRepositorio.ObtenerListaMenus().OrderBy(m => m.Nombre);
+            return View(listaMenus);
         }
+
+        public ActionResult Detalle(int idMenu)
+        {
+            var menu = menuRepositorio.ObtenerMenuPorId(idMenu);
+            if(menu == null)
+            {
+                return NotFound();
+            }
+            return View(menu);
+        }
+
+        public ActionResult Editar(int? idMenu)
+        {
+            TipoComidaSelectList = htmlHelper.GetEnumSelectList<TipoComida>();
+            
+            if (idMenu.HasValue)
+            {
+                Menu = menuRepositorio.ObtenerMenuPorId(idMenu.Value);
+            }
+            else
+            {
+                Menu = new Menu();
+            }
+            if(Menu == null)
+            {
+                return RedirectToPage("./NoSeEncontro");
+            }
+            return View(Menu);
+        }
+
     }
 }
